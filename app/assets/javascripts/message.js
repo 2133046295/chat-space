c076a3186f559ee8d88861b952c07d4367e52bce
2133046295 <61774517+2133046295@users.noreply.github.com>
2020-03-25 10:51:52 +0900

    非同期通信
    訂正完了

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,26 +39,26 @@ $(function(){
      return html;
    };
  }
-$('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);      
+      $('form')[0].reset();
+      
+    })
+    .always(function(){
+    $('.form__submit').attr('disabled', false);
+    })
  })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);      
-    $('form')[0].reset();
-    
-  })
-  .always(function(){
-  $('.form__submit').attr('disabled', false);
-  })
-})
 });
